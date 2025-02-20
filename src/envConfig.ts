478d938d895e4cf9a2537de96da6a177b6c1558d
basharkhan7776 <basharkhan7776@gmail.com>
2025-02-20 18:05:05 +0530

    Clear sensitive configuration values in envConfig.ts

--- src/envConfig.ts
+++ src/envConfig.ts
@@ -4,21 +4,21 @@ dotenv.config();
 
 
 
-export const uploadPreset="Profile_Upload";
-export const cloudName="dov4txbvf";
+export const uploadPreset="";
+export const cloudName="";
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyC-NyO450wSTnERU0LyG8kkliB1N8fh8QY",
-    authDomain: "api-culture-connect-5390e.firebaseapp.com",
-    projectId: "api-culture-connect-5390e",
-    storageBucket: "api-culture-connect-5390e.firebasestorage.app",
-    messagingSenderId: "612028648305",
-    appId: "1:612028648305:web:ff118f5424a023f67744d0"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 
-export const adminEmail="basharkhan7776@gmail.com";
+export const adminEmail="";
 
-export const SERVICE_ID="service_i4b2q4e";
-export const TEMPLATE_ID="template_4q84hmd";
-export const USER_ID="U9oo3Nuanm4n06mjC";
+export const SERVICE_ID="";
+export const TEMPLATE_ID="";
+export const USER_ID="";
