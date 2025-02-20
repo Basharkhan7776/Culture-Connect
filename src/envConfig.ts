a2b4ea567201a297cf4cff0dfd235a1477f1c8a3
basharkhan7776 <basharkhan7776@gmail.com>
2025-02-20 14:10:22 +0530

    Improve for error handling

--- src/envConfig.ts
+++ src/envConfig.ts
@@ -1,18 +1,25 @@
 
-export const uploadPreset="Profile_Upload";
-export const cloudName="dov4txbvf";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+
+export const uploadPreset = process.env.UPLOAD_PRESET;
+export const cloudName = process.env.CLOUD_NAME;
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyC-NyO450wSTnERU0LyG8kkliB1N8fh8QY",
-    authDomain: "api-culture-connect-5390e.firebaseapp.com",
-    projectId: "api-culture-connect-5390e",
-    storageBucket: "api-culture-connect-5390e.firebasestorage.app",
-    messagingSenderId: "612028648305",
-    appId: "1:612028648305:web:ff118f5424a023f67744d0"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
 };
 
-export const adminEmail="basharkhan7776@gmail.com";
 
-export const SERVICE_ID="service_i4b2q4e";
-export const TEMPLATE_ID="template_4q84hmd";
-export const USER_ID="U9oo3Nuanm4n06mjC";
+export const adminEmail = process.env.ADMIN_EMAIL;
+
+export const SERVICE_ID = process.env.EMAILJS_SERVICE_ID;
+export const TEMPLATE_ID = process.env.EMAILJS_TEMPLATE_ID;
+export const USER_ID = process.env.EMAILJS_USER_ID;
